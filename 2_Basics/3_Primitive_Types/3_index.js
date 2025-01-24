4773f5a6fad3629bc7b84d3ec1b4889cef8d6860
MU <mericajam@gmail.com>
2025-01-23 22:00:39 -0500

    Chapter 2 Basics  2 & 3 subsections just saved file

---
+++ 2_Basics/3_Primitive_Types/3_index.js
@@ -0,0 +1,7 @@
+// Primitive type -> String , Number, Boolean,undefined,null
+
+let name = 'Mohan'; // String literal
+let age = 30; // Number literal
+let isApproved = true; // Boolean literal
+let firstName = undefined; // undefined
+let lastName = null; // null
