1faa9d89651d35897e84a87d6f2de5ac376ec587
MU <mericajam@gmail.com>
2025-01-25 13:13:23 -0500

    Chapter 3 Basics  3 Arithmetic Operators

---
+++ 3_Operators/1_JavaScript_Operators/index.js
@@ -0,0 +1,32 @@
+//Types of Operators in Java Script
+// Arithmetic
+// Assignment
+// Comparison
+// Logical
+// Bitwise
+
+
+let x = 10;
+let y = 2;
+
+
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(x/y);
+console.log(x%y);
+console.log(x**y); // Exponential Operator
+
+
+// Increment ( ++ )
+console.log(++x)
+console.log(x)
+console.log(x++)
+
+// Decrement ( -- )
+console.log(--x)
+console.log(x)
+console.log(x--)
+
+
+
