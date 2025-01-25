aeae2ba39f083187e21af0c010043503124fd1f9
MU <mericajam@gmail.com>
2025-01-25 14:58:38 -0500

    Chapter 3 Basics  11 and 12 Subsections

---
+++ 3_Operators/11_Quiz_and_Exercise/index.js
@@ -0,0 +1,24 @@
+
+console.log('1' === 1);
+
+console.log(1 == 1);
+
+console.log(1 === 1);
+
+
+let x =(1 == true);
+
+console.log(x);
+
+console.log("-------------------------------------------")
+// Swap Variables Exercise
+
+let a = 'red';
+let b = 'blue';
+
+let temp = a;
+a = b;
+b = temp;
+
+console.log(a);
+console.log(b);
