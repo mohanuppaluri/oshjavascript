f14a2a83a796844b48d780a340fc54490460270b
MU <mericajam@gmail.com>
2025-02-24 12:18:21 -0500

    Minor Change

--- 2_Basics/7_Functions/7_index.js
+++ 2_Basics/7_Functions/7_index.js
@@ -3,5 +3,5 @@ function greet(name , lastName){
   console.log('Hello ' + name + ' ' + lastName);
 }
 
-greet('Mohan','Uppaluri');
-greet('Jyothsna',"Kamineni");
+greet('Sujata','Uppaluri');
+greet('Nageshwar Rao',"Uppaluri");
